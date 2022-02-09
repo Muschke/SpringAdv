250ad74ef3637ebe27c9a7bbea0b585f25635faf
Muschke <mussche.yannick@hotmail.be>
2022-02-09 16:58:38 +0100

    met test

--- src/main/java/be/vdab/muziekadvanced/restcontrollers/AlbumController.java
+++ src/main/java/be/vdab/muziekadvanced/restcontrollers/AlbumController.java
@@ -1,6 +1,7 @@
 package be.vdab.muziekadvanced.restcontrollers;
 
 import be.vdab.muziekadvanced.domain.Album;
+import be.vdab.muziekadvanced.domain.Track;
 import be.vdab.muziekadvanced.exceptions.AlbumNietGevondenException;
 import be.vdab.muziekadvanced.services.AlbumService;
 import org.springframework.hateoas.CollectionModel;
@@ -11,8 +12,6 @@ import org.springframework.hateoas.server.TypedEntityLinks;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.sound.midi.Track;
-import java.util.Collections;
 
 @RestController
 @RequestMapping("/albums")
@@ -27,15 +26,34 @@ class AlbumController {
     }
 
     @GetMapping("{id}")
+    /*het verwarrende aan deze oefening is dat je meermaals hetzelfde weergeeft op andere manier je moet:
+    * - albumnaam & artiestnaam teruggeven als string als je zoekt op het id van albumnummer
+    * - daarnaast moet je een link weergeven om naar effectieve albumgegevens te gaan, die gaan krak hetzelfde
+    * zijn als de eerste gegevens.*/
     EntityModel<AlbumNaamArtiestNaam> get(@PathVariable long id) {
+        /*ik wil dat mijn model het model van <ANAN> volgt, (zie private class), gebaseerd op id via url ingegeven*/
         return service.findById(id).map(album ->
+                /*zoek id op albumservice, je krijgt album van dat id terug en gaat dat voorts mappen*/
                         EntityModel.of(new AlbumNaamArtiestNaam(album))
+                                /*Ik wil dat je ons opgezocht album in deeltjes snijd(mappen) in de juiste delen in ons
+                                * ANAN class steekt, die mag je ook terugbrengen*/
                                 .add(links.linkToItemResource(album))
+                                /*Dan wil ik dat je daar links bijsteekt, deze staat letterlijk in cursus p60
+                                * onderaan pagina uitgelegd, dit bevat exact dezelfde data alsdat al wordt weergeven*/
                                 .add(links.linkForItemResource(album).slash( "tracks").withRel("tracks")))
+                /*Dan wil ik dat je daar tweede link bijsteekt.  Nu de link is voorgedaan, dus we nemen hetzelfde
+                * als de vorige links en voegen daar /tracks aan toe BAM link is klaar zegt de cursus.
+                * Dat is niet waar!! Die withRel is fout uitgelegd in de cursus. uw withRel moet die link
+                * nog specifiek benoemen anders dan 'self' of het werkt niet, hoe dat je die noemt is niet relevant
+                * nuttig is hetzelfde (bijvoorbeeld tracks) maar blabla werkt evengoed*/
                 .orElseThrow(AlbumNietGevondenException::new);
     }
+
     @GetMapping("{id}/tracks")
     CollectionModel<Track> getTracks(@PathVariable long id) {
+        /*voor embedd dingen weer te geven moet je CollectionModel gebruiken, de rest werkt exact hetzelfde
+        * let vooral op hoe met de links en de getmapping wordt gespeeld, dat moet je wel beethebben, dat moet juist zitten
+        * helaas zet de cursus weinig in op die controllers*/
         return service.findById(id).map(album ->
                         CollectionModel.of(album.getTracks())
                                 .add(links.linkForItemResource(album).slash("tracks").withRel("self"))
---
+++ src/test/java/be/vdab/muziekadvanced/restcontrollers/AlbumControllerTest.java
@@ -0,0 +1,46 @@
+package be.vdab.muziekadvanced.restcontrollers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql({"/insertArtiest.sql", "/insertAlbum.sql"})
+class AlbumControllerTest extends AbstractTransactionalJUnit4SpringContextTests {
+    private final MockMvc mvc;
+
+    public AlbumControllerTest(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    private long idVanTestAlbum() {
+        return jdbcTemplate.queryForObject(
+                "select id from albums where naam = 'testalbum'", Long.class);
+    }
+    @Test
+    void onbestaandeAlbumLezen() throws Exception {
+        mvc.perform(get("/albums/{id}", -1))
+                .andExpect(status().isNotFound());
+    }
+    @Test
+    void albumLezen() throws Exception{
+        var id = idVanTestAlbum();
+        mvc.perform(get("/albums/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("album").value("testalbum"));
+
+
+        //Je geeft aan jsonPath een JSONPath expressie mee. Je zoekt daarmee data in JSON data.
+           //     Je zoekt hier de waarde van het attribuut 'album'?? linea reacta met cursus zou ik naam of id zeggen .
+        //Die moet gelijk zijn aan waarde ...
+    }
+}
